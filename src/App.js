3cc32e53d2045e6c63340ac5c650081cf44d1a31
Segyul Park <65859360+Paaaark@users.noreply.github.com>
2023-09-23 11:07:12 +0000

    Update 2 files to show filters as a drawer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import "./styles.css";
 import { useState, useEffect } from 'react';
-import TopAppBar from './components/TopAppBar';
-import MainFragment from './components/MainFragment';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+// Import mui components
+import { ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
 import Drawer from '@mui/material/Drawer';
-import { getClient, fetchData } from './AWS';
+
+import { getClient, fetchData, fetchFilter } from './AWS';
+import myTheme from "./myTheme";
+import {findSubtotals} from './backend';
 import styles from './styles.css';
+
+// Import custom components
+import TopAppBar from './components/TopAppBar';
+import MainFragment from './components/MainFragment';
+import DrawerList from './components/DrawerList';
 
 export default function App() {
   const [cars, setCars] = useState(null);
   const [searchWord, setSearchWord] = useState("");
   const [filteredCars, setFilteredCars] = useState(null);
   const [drawerState, setDrawerState] = useState(false);
-
-  const defaultTheme = createTheme();
-
+  const [filters, setFilters] = useState(null);
   useEffect(() => {
     startData();
   }, []);
+
+  useEffect(() => {
+    setFilters(findSubtotals(cars));
+  }, [cars]);
 
   useEffect(() => {
     if (filteredCars != null) {
@@ -31,9 +40,10 @@ export default function App() {
 
   async function startData() {
     const myClient = await getClient();
-    const temp = await fetchData(myClient, "5");
-    setCars(temp);
-    setFilteredCars(temp);
+    const temp_car = await fetchData(myClient, "5");
+    const temp_filter = await fetchFilter(myClient);
+    setCars(temp_car);
+    setFilteredCars(temp_car);
   }
 
   const searchCar = (newWord) => {
@@ -45,10 +55,12 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={myTheme}>
       <TopAppBar searchCar={searchCar}></TopAppBar>
-      <Chip className="chip" label="Filters" variant="outlined" onClick={toggleDrawer}/>
-      <Drawer open={drawerState} onClose={toggleDrawer} />
+      <Chip color="secondary" label="Filters" className="chip" onClick={toggleDrawer}/>
+      <Drawer open={drawerState} onClose={toggleDrawer}>
+        <DrawerList />
+      </Drawer>
       <Grid>
         <MainFragment cars={filteredCars} searchWord={searchWord}/>
         {/* <CarCard car={{name: 'Alfa Romeo 4C Gr.4', brand: 'Alfa Romeo', pp: 610.55, country: 'Italy',
---
+++ src/components/DrawerList.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const DrawerList = ({}) => {
+    return (
+        <Box sx={{width: 250}} role="presentation">
+            <List>
+                {['Car Source', 'Make', "etc"].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    )
+}
+
+export default DrawerList;

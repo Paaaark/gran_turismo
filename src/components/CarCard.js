cea28051ed1f3713f56cab4bcc833f13b8129cc8
Segyul Park <65859360+Paaaark@users.noreply.github.com>
2023-09-28 00:51:50 +0000

    Make logo and car value size adjusted

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,29 +1,29 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { createTheme, styled } from '@mui/material/styles';
+
+// MUI Components
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
+
 import styles from './comp_styles.css';
-import { createTheme } from '@mui/material/styles';
 
 const CarCard = ( {car, flagImg, makeImg} ) => {
-    // const [car, setCar] = useState(car);
-
-    console.log("I got ", flagImg, " for ", car.name);
 
     return (
         <Card sx={{ width: "100%", height: "100%" }} style={{backgroundColor: '#141416'}}>
-            <CardContent>
+            <CardContent sx={{padding: '10px 0px 5px 10px'}}>
                 <Grid container direction='row' justifyContent='space-between' alignItems='center'>
                     <Grid item xs>
-                        <img width="10%" height="10%" src={makeImg} />
+                        <img height="30" src={makeImg} />
                     </Grid>
                     <Grid item xs>
                         <div className="rounded_rectangle" sx={{ align: "justify"}}>
-                            <Typography style={{fondWeight: 100}} align='center'>PP {car.pp}</Typography>
+                            <Typography sx={{fondWeight: 'bold'}} align='center'>PP {car.pp}</Typography>
                         </div>
                     </Grid>
                 </Grid>
